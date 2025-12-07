7d9cea92cd4f609b864b2bf593ae8127b63bd418
msguzman <19835670+msguzman@users.noreply.github.com>
2025-12-07 15:32:53 +0000

    Refactor DOMContentLoaded event listener

--- js/home.js
+++ js/home.js
@@ -48,11 +48,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
     counterObserver.observe(treeCounter);
 });
-
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
-
